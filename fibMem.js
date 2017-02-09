aea32901c74c00625b5970d4f6231e873752b028
David Parks <Davidthomasparks@gmail.com>
2017-02-08 23:57:57 -0800

    updated for eslint

--- fibMem.js
+++ fibMem.js
@@ -1,9 +1,9 @@
 
 let dictionary = {};
 let fibMem = (a) => {
-    if(a == 0 || a == 1) {
+    if (a == 0 || a == 1) {
         return 1;
-    } else if(a in dictionary) {
+    } else if (a in dictionary) {
         return dictionary[a];
     } else {
         dictionary[a] = fibMem(a - 1) + fibMem(a - 2);
