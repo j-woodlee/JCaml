ca5aa09cd2ccbbf96a2c3de3211f9bf5c0cc53df
Jake Woodlee <jwoodlee@lion.lmu.edu>
2017-03-01 23:39:29 -0800

    fixed some tabs

--- test/test.js
+++ test/test.js
@@ -7,22 +7,22 @@ const gram = ohm.grammar(jcaml);
 
 
 describe('Arithmetic', () => {
-  describe('3 + 1', () => {
-    it('should be equivalent to result', () => {
-      let match = gram.match('1 + 3');
-      assert.ok(match.succeeded());
+    describe('3 + 1', () => {
+        it('should be equivalent to result', () => {
+            let match = gram.match('1 + 3');
+            assert.ok(match.succeeded());
+        });
     });
-  });
-  describe('4 > 2', () => {
-    it('should be equivalent to result', () => {
-      let match = gram.match('4 > 2');
-      assert.ok(match.succeeded());
+    describe('4 > 2', () => { 
+        it('should be equivalent to result', () => {
+            let match = gram.match('4 > 2');
+            assert.ok(match.succeeded());
+        });
     });
-  });
-  describe('!5', () => {
-    it('should not be equivalent to result' , () => {
-      let match = gram.match('!5');
-      assert.ok(match.succeeded());
+    describe('!5', () => {
+        it('should not be equivalent to result', () => {
+            let match = gram.match('!5');
+            assert.ok(match.succeeded());
+        });
     });
-  });
 });
