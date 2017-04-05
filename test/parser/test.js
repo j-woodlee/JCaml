a5886e6c192292d6203adbc3c86e04ba1ae5c13d
Your Name <you@example.com>
2017-04-04 21:58:57 -0700

    Added to the test file.

--- test/parser/test.js
+++ test/parser/test.js
@@ -1,3 +1,19 @@
 const fs = require('fs');
 const assert = require('assert');
 const parse = require('../../parser.js');
+
+describe('The parser', () => {
+  fs.readdirSync(__dirname).forEach((name) => {
+    if (name.endsWith('.jml')) {
+      it(`produces the correct AST for ${name}`, (done) => {
+        fs.readFile(`${__dirname}/${name}`, 'utf-8', (err, input) => {
+          const ast = parse(input);
+          fs.readFile(`${__dirname}/${name}.json`, 'utf-8', (_err, expected) => {
+            assert.equal(JSON.stringify(ast), expected.trim());
+            done();
+          });
+        });
+      });
+    }
+  });
+});
