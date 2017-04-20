18a788d855e1fc70c14a319fbbedb8f0e6402766
DavidTParks <davidthomasparks@gmail.com>
2017-04-20 13:53:59 -0700

    removed console log

--- jcaml.js
+++ jcaml.js
@@ -55,6 +55,5 @@ fs.readFile(argv._[0], 'utf-8', (err, text) => {
     console.log(util.inspect(program, { depth: null }));
     return;
   }
-  console.log(program.constructor.prototype)
   program.gen();
 });
