9a36307177f416b036f4f083be894ceedca8eee3
Jake Woodlee <jwoodlee@lion.lmu.edu>
2017-04-25 13:34:00 -0700

    added analysis of list

--- ast/list.js
+++ ast/list.js
@@ -1,6 +1,17 @@
 module.exports = class List {
-    constructor(arg) {
-        this.arg = arg;
+    constructor(binexps) {
+        this.binexps = binexps;
+    }
+
+    analyze(context) {
+        const typeOfFirstElem = this.binexp[0].type;
+        this.binexps.forEach((binexp) => {
+            binexp.analyze(context);
+            if (binexp.type !== typeOfFirstElem) {
+                throw new Error("No heterogenous lists: " +
+                "All elements of a list must be of the same type");
+            }
+        });
     }
 
     toString() {
