abd272e85a8cdb736b136dd56657a9a520c81827
Jake Woodlee <jwoodlee@lion.lmu.edu>
2017-04-25 02:49:15 -0700

    added analyze function for numLit

--- ast/numLit.js
+++ ast/numLit.js
@@ -1,7 +1,15 @@
+const Type = require("./type");
+
 module.exports = class Numlit {
     constructor(value) {
       this.value = value;
     }
+
+    /* eslint-disable no-unused-vars*/
+    analyze(context) {
+        this.type = Type.INT;
+    }
+    /* eslint-enable */
 
     toString() {
         let numberString = "(numlit ";
