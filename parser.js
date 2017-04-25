bae61b6943677813a61d3b94558fdc0560ec9a97
Your Name <you@example.com>
2017-04-24 20:15:57 -0700

    modified parser and added analyzer test file

--- parser.js
+++ parser.js
@@ -533,7 +533,7 @@ const semantics = JCamlGrammar.createSemantics().addOperation("tree", {
     mullop(op) { return new Mullop(this.sourceString); },
     expop(op) { return new Expop(this.sourceString); },
     binop(op) { return new Binop(this.sourceString); },
-    FuncDec(_1, id, _2, params, _3, type, _4, body) {
+    FuncDec(_1, id, _2, params, _3, type, body) {
       return new FuncDec(id.sourceString, params.tree(), type.tree(), body.tree());
     },
     FuncCall(id, _1, args, _2) { return new FuncCall(id.sourceString, args.tree()); },
---
+++ test/analyzer/analyzer-test.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const assert = require("assert");
+const parse = require("../../parser.js");
+
+describe("The semantic analyzer", () => {
+  fs.readdirSync(__dirname).forEach((name) => {
+    if (name.endsWith(".error")) {
+      it(`detects a ${name.replace(/[^a-z]/g, " ")}`, (done) => {
+        const program = parse(fs.readFileSync(`${__dirname}/${name}`, "utf-8"));
+        const errorPattern = RegExp(name.replace(".error", "").replace(/-/g, " "), "i");
+        assert.throws(() => program.analyze(), errorPattern);
+        done();
+      });
+    } else if (name.endsWith(".jml")) {
+      it(`should analyze ${name} without errors`, (done) => {
+        // For now, we are happy to know that these files pass semantic analysis.
+        // We eventually need to check that the ASTs are properly decorated.
+        const program = parse(fs.readFileSync(`${__dirname}/${name}`, "utf-8"));
+        program.analyze();
+        done();
+      });
+    }
+  });
+});
