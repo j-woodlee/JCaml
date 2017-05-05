7ab1dd4d0b8ded502407605cee69382251e8b6ed
Your Name <you@example.com>
2017-05-04 18:38:48 -0700

    Minor adjustment to exp_ternary class.

--- ast/exp_ternary.js
+++ ast/exp_ternary.js
@@ -1,7 +1,7 @@
 const Type = require("./type");
 
 module.exports = class ExpTernary {
-    constructor(op, matchexp1, matchexp2, matchexp3) {
+    constructor(matchexp1, matchexp2, matchexp3) {
         this.matchexp1 = matchexp1;
         this.matchexp2 = matchexp2;
         this.matchexp3 = matchexp3;
